a24a5d902ea1f2de23ad057349d7b2a8b22d5e23
MeadowView <mv3142@gmail.com>
2018-12-02 09:27:18 -0800

    updated

---
+++ src/dart.py
@@ -0,0 +1,14 @@
+
+url_base = "http://dart.fss.or.kr/dsab002/search.ax?"
+field_dividend = "reportName"
+field_startdate = "startDate"
+field_enddate = "endDate"
+field_company = "textCrpNm"
+field_maxresult = "maxResult"
+
+
+field_
+
+def build
+
+
