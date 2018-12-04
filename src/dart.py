f2b6307718889b20540f4e96dde1e962cebe9b4d
MeadowView <mv3142@gmail.com>
2018-12-03 22:24:55 -0800

    updated

--- src/dart.py
+++ src/dart.py
@@ -3,6 +3,8 @@
 import requests
 import os
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import pandas as pd
 
 url_template = 'http://dart.fss.or.kr/dsac001/search.ax?selectDate=%s'
 headers = {'Cookie':'DSAC001_MAXRESULTS=5000;'}
@@ -41,12 +43,102 @@ class Fetcher:
             d = start + timedelta(days=i)
             rdate = d.strftime("%Y%m%d")
             filename = "DART-" + rdate + ".html"
-            self.wge(turl_template % rdate, self.savedir + filename)
+            self.wget(url_template % rdate, self.savedir + filename)
             fsize = os.path.getsize(self.savedir + filename)/(1024**2)
             print("%s (%.2f MB) downloaded" % (filename, fsize))
     
 
 
 
-f = Fetcher("/tmp/")
-f.get(0, 0, 0, 0)
+#f = Fetcher("/tmp/")
+#f.get(0, 0, 0, 0)
+
+fname = "/tmp/DART-20170405.html"
+
+with open(fname, encoding="utf-8") as f:
+    text = f.read()
+    soup = BeautifulSoup(text, "lxml")
+
+sel_list = soup.select('div p b')
+count_str = sel_list[0].text
+count_str = "".join(count_str.split())
+print(count_str)
+table = soup.find('table')
+trs = table.findAll('tr')
+print ("총 건수:", len(trs))
+for tr in trs[1:6]:
+    tds = tr.findAll('td')
+    print(tds[0].text.strip(), end=" " )          # 시간
+    print(tds[1].text.strip(), end=" " )          # 공시대상회사
+    print(tds[1].img['alt'])                      # 이미지 태그 (유가증권, 코스닥, 코넥스 등)
+    print(" ".join(tds[2].text.split()) )
+    print('http://dart.fss.or.kr/' + tds[2].a['href'] )   # 링크
+    print(tds[3].text.strip(), end=" ")                   # 제출인
+    print(tds[4].text.strip().replace('.', '-') )         # 접수일자
+    print()
+
+table = soup.find('table')
+trs = table.findAll('tr')     # <tr> </tr> : 1개의 공시.
+counts = len(trs)
+print(counts)
+
+tr = trs[1]
+print(tr)
+
+# 각 컬럼별 데이터 추출
+tds = tr.findAll('td')
+
+link = 'http://dart.fss.or.kr' + tds[2].a['href']
+doc_id = link.split('main.do?rcpNo=')[1]
+
+time = tds[0].text.strip()
+date = tds[4].text.strip().replace('.', '-') + ' ' + time
+
+corp_name = tds[1].text.strip()
+market = tds[1].img['alt']
+
+title = " ".join(tds[2].text.split())
+
+reporter = tds[3].text.strip()
+
+dart_dict = {'doc_id':[doc_id],'date':[date],'corp_name':[corp_name],
+'market':[market],'title':[title],'link':[link],'reporter':[reporter]}
+df = pd.DataFrame(dart_dict)
+print(df)
+
+# 전체 데이터를 DataFrame으로 만들기
+if counts > 0:
+    link_list = []
+    docid_list = []
+    date_list = []
+    corp_list = []
+    market_list = []
+    title_list = []
+    reporter_list = []
+    
+    for tr in trs[1:]:
+        tds = tr.findAll('td')
+        link = 'http://dart.fss.or.kr' + tds[2].a['href']
+        doc_id = link.split('main.do?rcpNo=')[1]
+        time = tds[0].text.strip()
+        date = tds[4].text.strip().replace('.', '-') + ' ' + time
+        corp_name = tds[1].text.strip()
+        market = tds[1].img['alt']
+        title = " ".join(tds[2].text.split())
+        reporter = tds[3].text.strip()
+        
+        link_list.append(link)
+        docid_list.append(doc_id)
+        date_list.append(date)
+        corp_list.append(corp_name)
+        market_list.append(market)
+        title_list.append(title)
+        reporter_list.append(reporter)
+        
+    dart_dict = {'doc_id':docid_list,'date':date_list,'corp_name':corp_list,'market':market_list,'title':title_list,
+                 'link':link_list,'reporter':reporter_list}
+    df_dart = pd.DataFrame(dart_dict)    
+
+print(df_dart.head())
+
+
