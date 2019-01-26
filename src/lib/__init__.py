00e2fc68f226ff29ad88adb1aaffb09bcbe335f3
CJ <mv3142@gmail.com>
2019-01-25 23:07:19 -0800

    updated

--- src/lib/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Finance package. """
-
-__all__ = [ ]
-
-# basic datatypes
-from .stock import Symbol
-from .stock import StockPrice
-from .stock import Market
--- src/run.py
+++
@@ -1,9 +0,0 @@
-import lib
-
-def main():
-        nyse = lib.Market("NYSE", "../data/nyse.csv")
-        nasdaq = lib.Market("NASDAQ", "../data/nasdaq.csv")
-
-if __name__ == '__main__':
-    main()
-
